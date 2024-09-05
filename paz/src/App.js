da53b341470363ce4e02a9b5b38d4e466df71ac6
haile <haileleulfiseha@gmail.com>
2024-09-05 17:57:33 +0300

    Update configurations and clean up unused files

--- paz/src/App.js
+++ paz/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Homepage from "./components/Homepage";
+// import Products from './components/Products'; // Example components for routing
+// import Gallery from './components/Gallery';
+// import Contact from './components/Contact';
+// import About from './components/About';
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<Navbar />
+
+				<Routes>
+					<Route path="/" element={<Homepage />} />
+					{/* <Route path="/products" element={<Products />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} /> */}
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
