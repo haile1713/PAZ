53b677cd36d85ad94fa0787e10f1d03ca1deeb66
haile <haileleulfiseha@gmail.com>
2024-09-17 13:18:47 +0300

    edited the descriptive texts

--- src/data/imageData.js
+++ src/data/imageData.js
@@ -1,14 +1,12 @@
 export const images = [
 	{
 		src: "/assets/trial image-4.png",
-		alt: "Beautiful Ceramic Collection",
 		topText: "PAZ MODERN TERRAZZO",
 		bottomText: '"DESIGNING YOUR PATH!"',
 	},
 	{
 		src: "/assets/imgae trial-7 (1).png",
-		alt: "Timeless Ceramic Designs",
-		topText: "Timeless to Contemporary",
-		bottomText: '"Paving your PAZ"',
+		topText: "TIMELESS TO CONTEMPORARY",
+		bottomText: '"PAVING YOUR PAZ"',
 	},
 ];
