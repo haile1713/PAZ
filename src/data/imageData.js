1f0219d1a3c7c270e24ce16f14981c40f155d57d
Hiwot2127 <hiwotbkm25@gmail.com>
2024-09-20 17:27:36 +0300

    Update page title and navbar text

--- src/data/imageData.js
+++ src/data/imageData.js
@@ -2,11 +2,11 @@ export const images = [
 	{
 		src: "/assets/trial image-4.png",
 		topText: "MODERN GRANITE AND TERRAZZO",
-		bottomText: '"PAVING YOUR PATH"',
+		bottomText: 'PAVING YOUR PATH',
 	},
 	{
 		src: "/assets/devine.jpg.webp",
 		topText: "TIMELESS TO CONTEMPORARY",
-		bottomText: '"REVOLUTIONARIZING ETHIOPIAN TILES AND TERRAZZO INDUSTRY"',
+		bottomText: 'REVOLUTIONARIZING ETHIOPIAN TILES AND TERRAZZO INDUSTRY',
 	},
 ];
