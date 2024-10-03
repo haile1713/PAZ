d53fa0837fba871c916dc205089b0854a7be37b4
fit-s-u-m <nehemiahmaths@gmail.com>
2024-10-03 17:38:52 +0300

    added another images and remove search feature

--- src/components/ProductCategory.jsx
+++ src/components/ProductCategory.jsx
@@ -1,23 +1,11 @@
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Label } from "@/components/ui/label";
 import { useState, useEffect } from "react";
-import { Button } from "@/components/ui/button"
-import {
-	Command,
-	CommandEmpty,
-	CommandGroup,
-	CommandInput,
-	CommandItem,
-	CommandList,
-} from "@/components/ui/command"
-import {
-	Popover,
-	PopoverContent,
-	PopoverTrigger,
-} from "@/components/ui/popover"
+import { Badge } from "@/components/ui/badge"
 
 const Category = ({ data, setFilter }) => {
 	const [selectedColor, setSelectedColor] = useState("any");
+	const [colorSet, setColorSet] = useState(() => new Set([]))
 
 	const pipe = (...fns) => (initialValue) => {
 		return fns.reduce((acc, fn) => fn(acc), initialValue);
@@ -25,27 +13,20 @@ const Category = ({ data, setFilter }) => {
 	const filterColor = (color) => (data) =>
 		data.filter((item) => item.color === color || color === "any");
 
-	const clean = (str) => str.toLowerCase().trim();
-	const doesInclude = (item1, item2) => {
-		return clean(item1).includes(clean(item2))
-	}
-
-	const searchProduct = (searchTerm) => (data) => {
-		if (!searchTerm) return data
-		return data.filter((item) => (doesInclude(item.title, searchTerm) || doesInclude(item.description, searchTerm)));
-	}
-
-	// for search
-	const [open, setOpen] = useState(false)
-	const [value, setValue] = useState("")
-
 	useEffect(() => {
 		const combinedFilter = pipe(
-			filterColor(selectedColor),
-			searchProduct(value)
+			filterColor(selectedColor)
 		);
 		setFilter(combinedFilter);
-	}, [selectedColor, value]);
+	}, [selectedColor]);
+	const pushItem = item => {
+		setColor(prev => new Set(prev).add(item));
+	}
+	useEffect(() => {
+		const newColorSet = new Set(data.map(item => item.color));
+		setColorSet(newColorSet);
+	}, [data]);
+
 
 
 	return (
@@ -53,16 +34,21 @@ const Category = ({ data, setFilter }) => {
 			<div className="color-category mt-5">
 				<h1 className="mb-3">Color</h1>
 				<RadioGroup defaultValue={selectedColor} onValueChange={x => setSelectedColor(x)}>
-					{data.map((item) => (
-						<div key={item.id} className="flex items-center space-x-2">
-							<RadioGroupItem id={item.title} value={item.color} style={{ color: item.hex }}>
-								<span className="sr-only">{item.color}</span> {/* For accessibility */}
-							</RadioGroupItem>
-							<Label htmlFor={item.title} style={{ color: item.hex }}>
-								{item.color}
-							</Label>
-						</div>
-					))}
+					{
+						Array.from(colorSet).map(color => {
+							const filterdItem = data.find(item => item.color == color)
+							if (!filterdItem) return null
+							return (
+								<div key={filterdItem.id} className="flex items-center space-x-2">
+									<RadioGroupItem id={filterdItem.title} value={filterdItem.color} style={{ color: filterdItem.hex }}>
+										<span className="sr-only">{filterdItem.color}</span> {/* For accessibility */}
+									</RadioGroupItem>
+									<Badge className="w-fit" style={{ backgroundColor: filterdItem.hex, color: filterdItem.hexBack }}>{filterdItem.color}</Badge>
+								</div>
+							)
+
+						})
+					}
 					<div className="flex items-center space-x-2">
 						<RadioGroupItem value="any" id="any-color">
 							<span className="sr-only">Any Color</span>
@@ -71,62 +57,6 @@ const Category = ({ data, setFilter }) => {
 					</div>
 				</RadioGroup>
 			</div>
-			<div className="search-product mt-10">
-				<Popover open={open} onOpenChange={setOpen}>
-					<PopoverTrigger asChild>
-						<Button
-							variant="outline"
-							role="combobox"
-							aria-expanded={open}
-							className="w-[200px] justify-between hover:text-secondary"
-						>
-							{value
-								? value
-								: "Select product..."}
-						</Button>
-					</PopoverTrigger>
-					<PopoverContent className="w-[200px] p-0">
-						<Command>
-							<CommandInput placeholder="Search product..." />
-							<CommandList>
-								<CommandEmpty>No product found.</CommandEmpty>
-								<CommandGroup>
-									{data.map((product) => (
-										<CommandItem
-											key={product.id}
-											value={product.title}
-											className="hover:text-secondary"
-											onSelect={(currentValue) => {
-												setValue(`${currentValue} `) // adding extra space makes it work why
-												setOpen(false)
-											}}
-										>
-											{product.title}
-										</CommandItem>
-									))}
-								</CommandGroup>
-								<CommandGroup>
-									{data.map((product) => (
-										<CommandItem
-											key={product.id}
-											value={product.description}
-											className="hover:text-secondary"
-											onSelect={(currentValue) => {
-												setValue(`${currentValue} `) // adding extra space makes it work why
-												setOpen(false)
-											}}
-										>
-											{product.description.slice(0, 15)}
-										</CommandItem>
-									))}
-								</CommandGroup>
-							</CommandList>
-						</Command>
-					</PopoverContent >
-				</Popover>
-
-			</div>
-
 		</>
 	);
 };
--- src/components/products.jsx
+++ src/components/products.jsx
@@ -14,13 +14,13 @@ const Product = ({ data }) => {
 			<Card className="mt-20" key={item.id}>
 				<CardHeader>
 					<CardTitle>{item.title}</CardTitle>
-					<Badge className="w-fit" style={{ backgroundColor: item.hex }}>{item.color}</Badge>
 				</CardHeader>
 				<CardContent>
 					<img src={item.img} className="h-60 w-60 xl:h-80 xl:w-80" alt={item.title} />
 				</CardContent>
 				<CardFooter>
 					<CardDescription>{item.description}</CardDescription>
+					<Badge className="w-fit" style={{ backgroundColor: item.hex, color: item.hexBack }}>{item.color}</Badge>
 				</CardFooter>
 			</Card>
 		))
