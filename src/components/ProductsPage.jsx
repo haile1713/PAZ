47f6f68deb18525be2d422cffd5fad63e9ea9f89
fit-s-u-m <nehemiahmaths@gmail.com>
2024-10-05 11:11:40 +0300

    added black backgound for contrast in product page

--- src/components/ProductsPage.jsx
+++ src/components/ProductsPage.jsx
@@ -10,7 +10,7 @@ const Products = () => {
 	}
 	return (
 		<div className="flex flex-col" id="products" >
-			<h1 className="text-3xl font-bold text-center mt-20">Products</h1>
+			<h1 className="text-3xl font-bold text-center mt-20 text-primary">Products</h1>
 			<div className="flex flex-row mt-10 gap-10">
 				<div className="w-1/6 ml-4"> {/* Adjust the width as needed */}
 					<Category data={data} setFilter={setFilter} />
