de4b454d29d78f85590c93b18b45b11a12ce0df2
Hiwot2127 <hiwotbkm25@gmail.com>
2024-10-05 21:50:48 +0300

    n

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "./components/Homepage";
 import Products from "./components/Products"; // Example components for routing
-import { ThemeProvider } from "@/components/theme-provider";
+
  import Gallery from './components/Gallery';
 // import Contact from './components/Contact';
 import AboutUs from './components/AboutUs';
